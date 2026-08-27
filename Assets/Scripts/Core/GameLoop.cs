9b755c6a53ac524e8566975cbeb2a21ccb45a811
Wilson <wilson@wilson.com>
2026-08-27 15:30:35 +0800

    Adjust platform positions
    Main: (8.5, 1.1, 5) size 20x3, collision Y=0.5
    Left: (3, 4, 5) size 5.2x2, collision Y=3.5
    Right: (15, 4, 5) size 5.2x2, collision Y=3.5

--- Assets/Scripts/Core/GameLoop.cs
+++ Assets/Scripts/Core/GameLoop.cs
@@ -173,7 +173,7 @@ namespace LABANAN
         {
             var platTex = Resources.Load<Texture2D>("Sprites/PLATFORM");
 
-            CreatePlatformVisual("PlatformVisuals", new Vector3(9f, 1f, 5), 20.5f, 3f, platTex, new Color(0.3f, 0.35f, 0.3f));
+            CreatePlatformVisual("PlatformVisuals", new Vector3(8.5f, 1.1f, 5), 20f, 3f, platTex, new Color(0.3f, 0.35f, 0.3f));
             CreatePlatformVisual("LeftPlatformVisual", new Vector3(3f, 4f, 5), 5.2f, 2f, platTex, new Color(0.35f, 0.3f, 0.3f));
             CreatePlatformVisual("RightPlatformVisual", new Vector3(15f, 4f, 5), 5.2f, 2f, platTex, new Color(0.3f, 0.3f, 0.35f));
 
